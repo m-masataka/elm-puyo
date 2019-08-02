1b28d287a496ea06b8bcf1ab1cad7a05e40b9687
m-masataka <m.mizukoshi.wakuwaku@gmail.com>
2019-08-02 20:03:12 +0900

    add score

--- src/Score.elm
+++ src/Score.elm
@@ -1,7 +1,6 @@
 module Score exposing (..)
 
 import List.Extra as ListEx
-import Debug
 
 chainBonus =
     [ 0, 0, 8, 16, 32, 64, 96, 128, 160, 192, 224, 256, 288, 320, 352, 384, 416, 448, 512]
@@ -13,9 +12,9 @@ calcScore: Int -> Int -> Int -> Int -> List Int -> Int
 calcScore score remove color chain link =
     score + (remove * 10 *
         ( 
-          (ListEx.getAt chain chainBonus |> Maybe.withDefault 0 |> Debug.log "chain")
-          + (List.map (\n -> n - 4 |> clamp 0 10) link |> List.sum  |> Debug.log "link")
-          + (ListEx.getAt color colorBonus |> Maybe.withDefault 0|> Debug.log "color")
+          (ListEx.getAt chain chainBonus |> Maybe.withDefault 0)
+          + (List.map (\n -> n - 4 |> clamp 0 10) link |> List.sum)
+          + (ListEx.getAt color colorBonus |> Maybe.withDefault 0)
           |> max 1
         )
     )
